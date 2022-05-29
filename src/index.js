85098dc10822ea96a87bc5c9a09a4d79cdc4d6ea
hhlwt <from.module.import@gmail.com>
2022-05-29 13:06:11 +0300

    step 3

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import fs from 'fs';
+import _ from 'lodash';
+
+const parseJsonData = (filePath) => {
+  const jsonData = fs.readFileSync(filePath, 'utf-8');
+  const parsedData = JSON.parse(jsonData);
+  return parsedData;
+};
+
+export default (filePath1, filePath2) => {
+  const obj1 = parseJsonData(filePath1);
+  const obj2 = parseJsonData(filePath2);
+  const [keys1, keys2] = [_.keys(obj1), _.keys(obj2)];
+  const uniqKeys = _.union(keys1, keys2).slice().sort();
+  const newObj = uniqKeys.reduce((newObj, key) => {
+    if (_.has(obj1, key) && !_.has(obj2, key)) {
+      newObj[`- ${key}`] = obj1[key];
+    }
+    if (!_.has(obj1, key) && _.has(obj2, key)) {
+      newObj[`+ ${key}`] = obj2[key];
+    }
+    if (_.has(obj1, key) && _.has(obj2, key)) {
+      if (obj1[key] === obj2[key]) {
+        newObj[`  ${key}`] = obj2[key];
+      }
+      if (obj1[key] !== obj2[key]) {
+        newObj[`- ${key}`] = obj1[key];
+        newObj[`+ ${key}`] = obj2[key];
+      }
+    }
+    return newObj;
+  }, {});
+  const string =
+    _.keys(newObj).reduce((acc, string) => {
+      const newAcc = acc + `  ${string}: ${newObj[string]}\n`;
+      return newAcc;
+    }, '{\n') + '}';
+
+  console.log(string);
+};
