eabf046b137f3ea86d61aade1db561af49ec2048
hhlwt <from.module.import@gmail.com>
2022-06-01 09:29:26 +0300

    add workflow

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,13 +2,6 @@ import { test, expect } from '@jest/globals';
 import genDiff from '../src/index.js';
 
 test('compare file1 file2', () => {
-  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json'))
-    .toBe(`{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`);
+  const expected = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
+  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toBe(expected);
 });
