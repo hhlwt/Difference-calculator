6c839ff37da1b96a2b7b31cf8cd514724eb9c88e
hhlwt <from.module.import@gmail.com>
2022-06-01 13:02:10 +0300

    update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,16 @@
 import { test, expect } from '@jest/globals';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
 test('compare file1 file2', () => {
-  const expected = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
+  const expected = readFile('expected_file.txt');
   expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json')).toBe(expected);
 });
