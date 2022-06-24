aa558dd9d3753d8369fb71f93e655c2ad2898b13
hhlwt <from.module.import@gmail.com>
2022-06-24 18:51:11 +0300

    plain formatter is done

--- formatters/plain.js
+++ formatters/plain.js
@@ -5,15 +5,20 @@ const plain = (tree) => {
     const strings = node.flatMap((item) => {
       const newPath = `${path}.${item.key}`;
       let string = '';
+      let value1;
+      let value2;
       switch (item.type) {
         case 'removed':
           string = `Property '${newPath.slice(1)}' was removed`;
           break;
         case 'added':
-          string = `Property '${newPath.slice(1)}' was added with value: ${_.isObject(item.value) ? '[complex value]' : item.value}`;
+          value1 = _.isObject(item.value) ? '[complex value]' : item.value;
+          string = `Property '${newPath.slice(1)}' was added with value: ${typeof value1 === 'string' && value1 !== '[complex value]' ? `'${value1}'` : `${value1}`}`;
           break;
         case 'changed':
-          string = `Property '${newPath.slice(1)}' was updated. From ${_.isObject(item.value.value1) ? '[complex value]' : item.value.value1} to ${_.isObject(item.value.value2) ? '[complex value]' : item.value.value2}`;
+          value1 = _.isObject(item.value.value1) ? '[complex value]' : item.value.value1;
+          value2 = _.isObject(item.value.value2) ? '[complex value]' : item.value.value2;
+          string = `Property '${newPath.slice(1)}' was updated. From ${typeof value1 === 'string' && value1 !== '[complex value]' ? `'${value1}'` : `${value1}`} to ${typeof value2 === 'string' && value2 !== '[complex value]' ? `'${value2}'` : `${value2}`}`;
           break;
         case 'nested':
           string = iter(item.value, newPath);
