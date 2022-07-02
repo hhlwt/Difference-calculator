984b988560fd3144c886e7f3598ced89078863d0
hhlwt <from.module.import@gmail.com>
2022-07-02 17:16:35 +0300

    fix refactoring issue

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -10,13 +10,32 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test.each([
-  ['./__fixtures__/file1.json', './__fixtures__/file2.json', 'stylish', readFile('expected_file_stylish.txt')],
-  ['./__fixtures__/file3.yml', './__fixtures__/file4.yml', 'stylish', readFile('expected_file_stylish.txt')],
-  ['./__fixtures__/file1.json', './__fixtures__/file2.json', 'plain', readFile('expected_file_plain.txt')],
-  ['./__fixtures__/file3.yml', './__fixtures__/file4.yml', 'plain', readFile('expected_file_plain.txt')],
-  ['./__fixtures__/file1.json', './__fixtures__/file2.json', 'json', readFile('expected_file_json.txt')],
-  ['./__fixtures__/file3.yml', './__fixtures__/file4.yml', 'json', readFile('expected_file_json.txt')],
-])('compare (%s, %s) with %s formatter', (first, second, format, expected) => {
-  expect(genDiff(first, second, format)).toBe(expected);
+test('compare json STYLISH', () => {
+  const expectedJson = readFile('expected_file_stylish.txt');
+  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json', 'stylish')).toBe(expectedJson);
+});
+
+test('compare yml STYLISH', () => {
+  const expectedYml = readFile('expected_file_stylish.txt');
+  expect(genDiff('./__fixtures__/file3.yml', './__fixtures__/file4.yml', 'stylish')).toBe(expectedYml);
+});
+
+test('compare json PLAIN', () => {
+  const expectedJson = readFile('expected_file_plain.txt');
+  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json', 'plain')).toBe(expectedJson);
+});
+
+test('compare yml PLAIN', () => {
+  const expectedYml = readFile('expected_file_plain.txt');
+  expect(genDiff('./__fixtures__/file3.yml', './__fixtures__/file4.yml', 'plain')).toBe(expectedYml);
+});
+
+test('compare json JSON', () => {
+  const expectedJson = readFile('expected_file_json.txt');
+  expect(genDiff('./__fixtures__/file1.json', './__fixtures__/file2.json', 'json')).toBe(expectedJson);
+});
+
+test('compare yml JSON', () => {
+  const expectedYml = readFile('expected_file_json.txt');
+  expect(genDiff('./__fixtures__/file3.yml', './__fixtures__/file4.yml', 'json')).toBe(expectedYml);
 });
