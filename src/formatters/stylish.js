759c4813a9469fcffffedd46b0be718f68769ab7
hhlwt <from.module.import@gmail.com>
2022-07-29 08:20:15 +0300

    refactoring

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -3,27 +3,30 @@ import _ from 'lodash';
 const getIndent = (depth, replacersCount = 4) => ' '.repeat(depth * replacersCount - 2);
 const getBracketIndent = (depth, replacersCount = 4) => ' '.repeat(replacersCount * depth - replacersCount);
 
-const stringifyValue = (nodeValue, depth, iterFunc) => {
+const stringifyValue = (nodeValue) => {
   if (!_.isPlainObject(nodeValue)) {
     return String(nodeValue);
   }
   const result = Object.entries(nodeValue).map(([key, value]) => ({ key, value, type: 'unchanged' }));
 
-  return iterFunc(result, depth + 1);
+  return result;
 };
 
 const stylish = (tree) => {
   const iter = (subTree, depth = 1) => {
+    if (typeof subTree === 'string') {
+      return subTree;
+    }
     const strings = subTree.map((node) => {
       switch (node.type) {
         case 'added':
-          return `${getIndent(depth)}+ ${node.key}: ${stringifyValue(node.value, depth, iter)}`;
+          return `${getIndent(depth)}+ ${node.key}: ${iter(stringifyValue(node.value), depth + 1)}`;
         case 'removed':
-          return `${getIndent(depth)}- ${node.key}: ${stringifyValue(node.value, depth, iter)}`;
+          return `${getIndent(depth)}- ${node.key}: ${iter(stringifyValue(node.value), depth + 1)}`;
         case 'changed':
-          return `${getIndent(depth)}- ${node.key}: ${stringifyValue(node.value1, depth, iter)}\n${getIndent(depth)}+ ${node.key}: ${stringifyValue(node.value2, depth, iter)}`;
+          return `${getIndent(depth)}- ${node.key}: ${iter(stringifyValue(node.value1), depth + 1)}\n${getIndent(depth)}+ ${node.key}: ${iter(stringifyValue(node.value2), depth + 1)}`;
         case 'unchanged':
-          return `${getIndent(depth)}  ${node.key}: ${stringifyValue(node.value, depth, iter)}`;
+          return `${getIndent(depth)}  ${node.key}: ${iter(stringifyValue(node.value), depth + 1)}`;
         case 'nested':
           return `${getIndent(depth)}  ${node.key}: ${iter(node.children, depth + 1)}`;
         default:
